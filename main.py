fda10e363b321965f5aaea6c127deb7c70c02f2a
Jake Sexton <drained77@gmail.com>
2023-01-04 11:22:00 -0500

    Updated the application with conditional statments that allows the user to choose whether they want to add, show, or exit. Also added a for loop that makes the list appear cleaner.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-user_prompt = "Enter a todo:"
-
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo)
-    print(todos)
+    user_action = input("Type add, show, or exit: ")
+    user_action = user_action.strip()
     
+    if user_action == 'add':
+        todo = input("Enter a todo: ")
+        todos.append(todo)
+    elif user_action == 'show':
+        for item in todos:
+            print(item)
+    elif user_action == 'exit':
+        break
+print("Bye")
 
 
