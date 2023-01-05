3ebd51834592d574f1e1ff632187f60c6aaef428
Jake Sexton <drained77@gmail.com>
2023-01-05 08:16:37 -0500

    Added an edit feature today that allows you to choose the item in the list, based on it's number and replace it with a new item.

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, or exit: ")
     user_action = user_action.strip()
     
-    if user_action == 'add':
+    if user_action == "add":
         todo = input("Enter a todo: ")
         todos.append(todo)
-    elif user_action == 'show':
+    elif user_action == "show":
         for item in todos:
             print(item)
-    elif user_action == 'exit':
+    elif user_action == "edit":
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
+        new_todo = input("Enter new todo: ")
+        todos[number] = new_todo
+    elif user_action == "exit":
         break
+    else:
+        print("Please choose add, show, or exit!")
+
 print("Bye")
 
 
