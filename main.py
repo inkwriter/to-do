0c10bc186ea3253d178f5f11e861bc2e1f1f8097
Jake Sexton <drained77@gmail.com>
2023-01-09 09:23:21 -0500

    Day 5, I added an exit feature and a complete feature that removes a list item based on the number that it was in the list. I also added one to the number variable to make it more user friendly, that it would start from one now, instead of zero.

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit, or exit: ")
+    user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
     
     if user_action == "add":
         todo = input("Enter a todo: ")
         todos.append(todo)
     elif user_action == "show":
-        for item in todos:
-            print(item)
+        for index, item in enumerate(todos):
+            row = f"{index +1} -{item}"
+            print(row)
     elif user_action == "edit":
         number = int(input("Number of the todo to edit: "))
         number = number - 1
         new_todo = input("Enter new todo: ")
         todos[number] = new_todo
+    elif user_action == "complete":
+        number = int(input("Number of the todo to complete: "))
+        todos.pop(number -1)
     elif user_action == "exit":
         break
     else:
-        print("Please choose add, show, or exit!")
+        print("Please choose add, show, edit, or exit!")
 
 print("Bye")
 
