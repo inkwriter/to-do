f5ee83383be213d3cff86e6287b40cd689c163d2
Jake Sexton <drained77@gmail.com>
2023-01-11 13:10:08 -0500

    Day 8, updated the edit and complete features so that they wouldn't just interact with the list, but the text file as well.

--- main.py
+++ main.py
@@ -7,20 +7,18 @@ while True:
     if user_action == "add":
         todo = input("Enter a todo: ") + "\n"
 
-        file = open("todos.txt", "r")
-        todos = file.readlines()
-        file.close()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
         todos.append(todo)
 
-        file = open("todos.txt", "w")
-        file.writelines(todos)
-        file.close()
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
     elif user_action == "show":
-        file = open("todos.txt", "r")
-        todos = file.readlines()
-        file.close()
-        
+                
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+
         new_todos = []
 
         for item in todos:
@@ -35,11 +33,30 @@ while True:
     elif user_action == "edit":
         number = int(input("Number of the todo to edit: "))
         number = number - 1
+
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+
         new_todo = input("Enter new todo: ")
-        todos[number] = new_todo
+        todos[number] = new_todo + "\n"
+
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
+
     elif user_action == "complete":
-        number = int(input("Number of the todo to complete: "))
-        todos.pop(number -1)
+        number = int(input("The number that was removed from the list: "))
+
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        index = number - 1
+        todo_to_remove = todos[index].strip("\n")
+        todos.pop(index)
+
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
+
+        message = f"Todo {todo_to_remove} was removed from the list."
+
     elif user_action == "exit":
         break
     else:
